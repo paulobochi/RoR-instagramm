441a5d1aa82a0bc208f3299725717ef166e5921a
Paulo Medeiros <pauloricardodemedeiros@hotmail.com>
2016-05-15 17:15:17 -0300

    Add devise and associations with Pics

--- app/controllers/pics_controller.rb
+++ app/controllers/pics_controller.rb
@@ -3,24 +3,40 @@ class PicsController < ApplicationController
   before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
-    @pics = Pic.all
+    @pics = Pic.all.order('created_at DESC')
   end
 
   def show
   end
 
   def new
-    @pic = Pic.new
+    @pic = current_user.pics.new
   end
 
   def create
-    @pic = Pic.new(pic_params)
+    @pic = current_user.pics.new(pic_params)
 
     if @pic.save
       redirect_to @pic, notice: 'Yess! It was posted!'
     else
       render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @pic.update(pic_params)
+      redirect_to @pic, notice: 'Congrats! Pic was updated!'
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @pic.destroy
+    redirect_to root_path
   end
 
   private
--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,2 +1,5 @@
 class Pic < ActiveRecord::Base
+
+  belongs_to :user
+  
 end
